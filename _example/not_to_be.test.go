787ec138cbb6a825bdc47e8cf4ccbcd111c175f2
shinshin86 <beagles1986@gmail.com>
2022-05-20 07:13:15 +0900

    Add assert not equal

--- _example/not_to_be.test.go
+++ _example/not_to_be.test.go
@@ -14,5 +14,77 @@ func main() {
 		})
 	})
 
+	t.It("Expect i is not 1", func() {
+		i := 1
+		t.Expect(i).NotToBe(2)
+	})
+
+	t.It("Expect f is not 1.2", func() {
+		f := 1.2
+		t.Expect(f).NotToBe(1.3)
+	})
+
+	t.It("Expect f is not 1.23", func() {
+		f := 1.23
+		t.Expect(f).NotToBe(1.24)
+	})
+
+	t.It("Expect f is not 1.234", func() {
+		f := 1.234
+		t.Expect(f).NotToBe(1.235)
+	})
+
+	t.It("Expect f is not 1.2345", func() {
+		f := 1.2345
+		t.Expect(f).NotToBe(1.2346)
+	})
+
+	t.It("Expect f is not 1.23456", func() {
+		f := 1.23456
+		t.Expect(f).NotToBe(1.23457)
+	})
+
+	t.It("Expect s is not helloworld", func() {
+		s := "helloworld"
+		t.Expect(s).NotToBe("helloworldd")
+	})
+
+	t.It("Expect r is not a", func() {
+		r := 'a'
+		t.Expect(r).NotToBe('b')
+	})
+
+	t.It("Expect b is not abc", func() {
+		b := []byte("abc")
+		t.Expect(b).NotToBe([]byte("abd"))
+	})
+
+	t.It("Expect b is not abc([]byte{97, 98, 99}", func() {
+		b := []byte("abc")
+		t.Expect(b).NotToBe([]byte{97, 98, 100})
+	})
+
+	t.It("Expect a is not [1 2]", func() {
+		var a [2]int
+		a[0] = 1
+		a[1] = 2
+		t.Expect(a).NotToBe([2]int{1, 3})
+	})
+
+	t.It("Expect a is not [Hello World]", func() {
+		var a [2]string
+		a[0] = "Hello"
+		a[1] = "World"
+		t.Expect(a).NotToBe([2]string{"Hello", "Worldd"})
+	})
+
+	t.It("Expect p is not ", func() {
+		i := 123
+		i2 := 123
+		p := &i
+		p2 := &i2
+		t.Expect(p).NotToBe(p2)
+	})
+
 	t.Exit()
 }
--- te/expect.go
+++ te/expect.go
@@ -54,6 +54,18 @@ func assertDeepEqual[T CompareArrayType](actual, expect T, testName string) bool
 	}
 }
 
+func assertNotDeepEqual[T CompareArrayType](actual, expect T, testName string) bool {
+	if !reflect.DeepEqual(actual, expect) {
+		DisplaySuccessMessage("Succeeded", 4)
+		return true
+	} else {
+		msg := fmt.Sprintf("Failed!: " + testName + "\n")
+		msg = msg + fmt.Sprintf("    Actual: %v, Expected: %v", actual, expect)
+		DisplayFailMessage(msg, 4)
+		return false
+	}
+}
+
 func assertNotEqual[T CompareType](actual, expect T, testName string) bool {
 	if actual != expect {
 		DisplaySuccessMessage("Succeeded", 4)
@@ -84,6 +96,143 @@ func (t *Te) NotToBe(i interface{}) {
 		if assertNotEqual(actual, expect, t.CurrentTestName) {
 			t.testSuccessCnt++
 		} else {
+			t.testFailCnt++
+		}
+	case reflect.Int:
+		actual, ok := t.CurrentTestValue.(int)
+		if ok == false {
+			t.notExpectedTypeMsg()
+			t.exitCode = 1
+		}
+
+		expect, ok := i.(int)
+		if ok == false {
+			notExpectedTypeMsg(i)
+			t.exitCode = 1
+		}
+
+		if assertNotEqual(actual, expect, t.CurrentTestName) {
+			t.testSuccessCnt++
+		} else {
+			t.testFailCnt++
+		}
+	case reflect.Float64:
+		actual, ok := t.CurrentTestValue.(float64)
+		if ok == false {
+			t.notExpectedTypeMsg()
+			t.exitCode = 1
+		}
+
+		expect, ok := i.(float64)
+		if ok == false {
+			notExpectedTypeMsg(i)
+			t.exitCode = 1
+		}
+
+		if assertNotEqual(actual, expect, t.CurrentTestName) {
+			t.testSuccessCnt++
+		} else {
+			t.testFailCnt++
+		}
+	case reflect.String:
+		actual, ok := t.CurrentTestValue.(string)
+		if ok == false {
+			t.notExpectedTypeMsg()
+			t.exitCode = 1
+		}
+
+		expect, ok := i.(string)
+		if ok == false {
+			notExpectedTypeMsg(i)
+			t.exitCode = 1
+		}
+
+		if assertNotEqual(actual, expect, t.CurrentTestName) {
+			t.testSuccessCnt++
+		} else {
+			t.testFailCnt++
+		}
+	case reflect.Int32: // rune
+		actual, ok := t.CurrentTestValue.(rune)
+		if ok == false {
+			t.notExpectedTypeMsg()
+			t.exitCode = 1
+		}
+
+		expect, ok := i.(rune)
+		if ok == false {
+			notExpectedTypeMsg(i)
+			t.exitCode = 1
+		}
+
+		if assertNotEqual(actual, expect, t.CurrentTestName) {
+			t.testSuccessCnt++
+		} else {
+			t.testFailCnt++
+		}
+	case reflect.Slice:
+		switch i.(type) {
+		case []uint8:
+			actual, ok := t.CurrentTestValue.([]uint8)
+			if ok == false {
+				t.notExpectedTypeMsg()
+				t.exitCode = 1
+			}
+
+			expect, ok := i.([]uint8)
+			if ok == false {
+				notExpectedTypeMsg(i)
+				t.exitCode = 1
+			}
+
+			if assertNotDeepEqual(actual, expect, t.CurrentTestName) {
+				t.testSuccessCnt++
+			} else {
+				t.testFailCnt++
+			}
+		}
+	case reflect.Array:
+		actual := reflect.ValueOf(t.CurrentTestValue)
+		expect := reflect.ValueOf(i)
+
+		if actual.Len() != expect.Len() {
+			msg := fmt.Sprintf("Error: The number of arrays to be compared does not match.\n")
+			DisplayFailMessage(msg, 4)
+			t.exitCode = 1
+		}
+
+		itemLen := actual.Len()
+		equalCount := 0
+
+		for idx := 0; idx < actual.Len(); idx++ {
+			actualItem := actual.Index(idx).Interface()
+			expectItem := expect.Index(idx).Interface()
+
+			if actualItem == expectItem {
+				equalCount++
+			}
+		}
+
+		if itemLen == equalCount {
+			msg := fmt.Sprintf("Failed!: " + t.CurrentTestName + "\n")
+			msg = msg + fmt.Sprintf("    Actual: %v, Not expected: %v", actual, expect)
+			DisplayFailMessage(msg, 4)
+			t.testFailCnt++
+		} else {
+			DisplaySuccessMessage("Succeeded", 4)
+			t.testSuccessCnt++
+		}
+	case reflect.Pointer:
+		actual := reflect.ValueOf(t.CurrentTestValue)
+		expect := reflect.ValueOf(i)
+
+		if !reflect.DeepEqual(actual, expect) {
+			DisplaySuccessMessage("Succeeded", 4)
+			t.testSuccessCnt++
+		} else {
+			msg := fmt.Sprintf("Failed!: " + t.CurrentTestName + "\n")
+			msg = msg + fmt.Sprintf("    Actual: %s, Expected: %s", actual, expect)
+			DisplayFailMessage(msg, 4)
 			t.testFailCnt++
 		}
 	default:
