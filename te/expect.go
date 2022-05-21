286ca78fe2423b876260ba86bd080b8e31aa0880
shinshin86 <beagles1986@gmail.com>
2022-05-22 07:21:45 +0900

    Add assert.go

--- te/expect.go
+++ te/expect.go
@@ -30,54 +30,6 @@ func notExpectedTypeMsg(i interface{}) {
 	DisplayFailMessage(msg, 4)
 }
 
-func assertEqual[T CompareType](actual, expect T, testName string) bool {
-	if actual == expect {
-		DisplaySuccessMessage("Succeeded", 4)
-		return true
-	} else {
-		msg := fmt.Sprintf("Failed!: " + testName + "\n")
-		msg = msg + fmt.Sprintf("    Actual: %v, Expected: %v", actual, expect)
-		DisplayFailMessage(msg, 4)
-		return false
-	}
-}
-
-func assertDeepEqual[T CompareArrayType](actual, expect T, testName string) bool {
-	if reflect.DeepEqual(actual, expect) {
-		DisplaySuccessMessage("Succeeded", 4)
-		return true
-	} else {
-		msg := fmt.Sprintf("Failed!: " + testName + "\n")
-		msg = msg + fmt.Sprintf("    Actual: %v, Expected: %v", actual, expect)
-		DisplayFailMessage(msg, 4)
-		return false
-	}
-}
-
-func assertNotDeepEqual[T CompareArrayType](actual, expect T, testName string) bool {
-	if !reflect.DeepEqual(actual, expect) {
-		DisplaySuccessMessage("Succeeded", 4)
-		return true
-	} else {
-		msg := fmt.Sprintf("Failed!: " + testName + "\n")
-		msg = msg + fmt.Sprintf("    Actual: %v, Expected: %v", actual, expect)
-		DisplayFailMessage(msg, 4)
-		return false
-	}
-}
-
-func assertNotEqual[T CompareType](actual, expect T, testName string) bool {
-	if actual != expect {
-		DisplaySuccessMessage("Succeeded", 4)
-		return true
-	} else {
-		msg := fmt.Sprintf("Failed!: " + testName + "\n")
-		msg = msg + fmt.Sprintf("    Actual: %v, Not expected: %v", actual, expect)
-		DisplayFailMessage(msg, 4)
-		return false
-	}
-}
-
 func (t *Te) NotToBe(i interface{}) {
 	switch t.CurrentTestType {
 	case reflect.Bool:
